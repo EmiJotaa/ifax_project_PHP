5ca9c9f5ad00ad415ebffd80e72bea57b6b31e08
Marco Júnior <mrcez4rr@gmail.com>
2024-12-11 18:53:29 -0300

    Section Slider_banner and CSS

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,3 +1,23 @@
 <?=$this->fetch('commons/header.php', $data)?>
-
+<section class="slider_banner">
+	<div class="conteudo">
+		<div class="itens">
+			<div class="item">
+				<a href="">
+					<img src="<?=URL_BASE?>resources/imagens/banner.png">
+				</a>
+			</div>
+			<div class="item">
+				<a href="https://www.google.com/maps/place/Parque+Indígena+do+Xingu/@-11.6627729,-52.8679238,8z/data=!4m6!3m5!1s0x930fbd3a77425f3f:0x91369ea8e1ff4631!8m2!3d-11.2468098!4d-53.2070192!16s%2Fm%2F027wmyf?entry=ttu&g_ep=EgoyMDI0MTIwOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">
+					<img src="<?=URL_BASE?>resources/imagens/banner1.png">
+				</a>
+			</div>
+			<div class="item">
+				<a href="">
+					<img src="<?=URL_BASE?>resources/imagens/banner3.png">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
 <?=$this->fetch('commons/footer.php', $data)?>
