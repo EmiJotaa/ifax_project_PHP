e11847e656270eaeb1eea66536287f6f044463fd
Marco Júnior <mrcez4rr@gmail.com>
2024-12-11 18:58:55 -0300

    Section Call_to_action and CSS

--- src/Views/home.php
+++ src/Views/home.php
@@ -41,4 +41,15 @@
 		</div>
 	</div>
 </section>
+<section class="call_to_action">
+	<video autoplay muted loop>
+		<source src="<?=URL_BASE?>resources/imagens/video-background.mp4" type="video/mp4">
+	</video>
+	<div class="container">
+		<div class="conteudo">
+			<h1>Somos uma instituição da família do xingu</h1>
+			<a href="<?=URL_BASE?>donate" class="btn">Fazer Doação</a>
+		</div>
+	</div>
+</section>
 <?=$this->fetch('commons/footer.php', $data)?>
